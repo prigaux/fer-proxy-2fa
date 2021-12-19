cfbe1ac780c27abee6d246407f32b1b7f069e8a1
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2021-12-19 10:40:55 +0100

    rotate-sealer.sh: force our IDP_HOME

--- bin/rotate-sealer.sh
+++ bin/rotate-sealer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 ## Renouvellement de la clé AES de l'IdP Shibboleth
-IDP_HOME=/opt/shibboleth-idp
+IDP_HOME=/webhome/idp/fer-proxy-2fa
 #export JAVA_HOME=/usr/java/latest/
  
 $IDP_HOME/bin/seckeygen.sh \
